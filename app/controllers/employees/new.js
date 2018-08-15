b99b0635b42ca06fde17d6b48382407050846a75
saurabh30 <gupta.saurabh711@gmail.com>
2018-08-15 21:28:32 +0530

    add working

---
+++ app/controllers/employees/new.js
@@ -0,0 +1,18 @@
+import Controller from '@ember/controller';
+
+export default Controller.extend({
+    actions:{
+        add(){
+            
+            var employee= this.get('store').createRecord('employee', {
+            contact:this.get('eContact'),
+            name :this.get('eName'),
+            location:this.get('eCity')
+            });
+            this.transitionToRoute('employees');
+            employee.save();
+
+            
+        }
+    }
+});
