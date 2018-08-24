b364bb4bc182bfcf584fc8047b6067e00112bcba
saurabh30 <saurabh.gupta4@synapse.com>
2018-08-24 16:16:10 +0530

    update model done

--- app/controllers/employees/show.js
+++ app/controllers/employees/show.js
@@ -1,19 +1,18 @@
 import Controller from '@ember/controller';
 export default Controller.extend({
-    
+   
     actions:{
         show(){
+            
              return {
                name : this.get('model.name'),
                contact : this.get('model.contact'),
                location : this.get('model.location'),
             }
         },
-        edit(){
+        edit(name, contact, location){
+            
             var self = this;
-            var name = this.get('model.name');
-            var contact = this.get('model.contact');
-            var location = this.get('model.location');
             this.get('store').findRecord('employee',this.get('model.id')).then(function(employee){
                   employee.set('name',name);
                   employee.set('contact',contact);
